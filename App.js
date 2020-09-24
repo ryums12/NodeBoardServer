c37b97a4ab9f5d37a65a15cf13f64aea60017e1d
minsongryu <ryums0712@archivsoft.com>
2020-09-24 16:33:38 +0900

    put complete

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
 
 app.get('/', (req, res) => {
 
 });
 
 app.put('/boards', (req, res) => {
+    console.log('Here is Put');
+    const query = "insert into board (title, note, reg_dt) values (?, ?, now())"
+    const title = req.body.title;
+    const note = req.body.note;
+    const params = [title, note];
+
+    connection.query(query, params,
+        (err, rows) => {
+            if(err) {
+                res.send(err);
+            } else {
+                res.send(rows);
+            }
+        }
+    );
+});
+
+app.get('/boards/:idx', (req, res) => {
 
 });
 
@@ -14,9 +49,6 @@ app.delete('/boards/:idx', (req, res) => {
 
 });
 
-app.get('/boards/:idx', (req, res) => {
-
-});
 
 app.listen(3000, () => {
     console.log("Express server has started on port 3000")
